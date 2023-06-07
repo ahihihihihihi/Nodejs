81c39f91e577c198fdf719abc0d94675868b893f
ahihi <bdsthocu20@gmail.com>
2023-06-07 11:46:17 +0700

    #58.1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -63,8 +63,12 @@ let handleEditUser = async (req, res) => {
 
 let getAllCode = async (req, res) => {
     try {
-        let data = await userService.getAllCodeService(req.query.type);
-        return res.status(200).json(data)
+        setTimeout(async () => {
+            let data = await userService.getAllCodeService(req.query.type);
+            return res.status(200).json(data)
+        }, 0)
+
+
     } catch (e) {
         console.log('Get all code error: ', e);
         return res.status(200).json({
