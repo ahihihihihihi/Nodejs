f790baae72859ff69b5190e163c5c4d3d27afc8b
ahihi <bdsthocu20@gmail.com>
2023-06-15 23:51:43 +0700

    #63.2

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -1,9 +1,12 @@
 import doctorService from "../services/doctorService"
 
 let getTopDoctorHome = async (req, res) => {
-    let limit = req.query.limit;
-    if (!limit) limit = 10;
+    let limit = parseInt(req.query.limit);
+    // console.log('req.query.limit = ', limit);
+    limit ? limit : (limit = 10);
+    // console.log('req.query.limit = ahihi...', limit);
     try {
+        // console.log('limit console: ', limit);
         let response = await doctorService.getTopDoctorHome(limit);
         return res.status(200).json(response);
     } catch (e) {
