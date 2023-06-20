cf98f89c299769fed3490bbc1c848619d6e0daed
ahihi <bdsthocu20@gmail.com>
2023-06-20 16:58:49 +0700

    #67

--- src/route/web.js
+++ src/route/web.js
@@ -25,6 +25,9 @@ let initWebRoutes = (app) => {
 
     router.get('/api/top-doctor-home', doctorController.getTopDoctorHome);
 
+    router.get('/api/get-all-doctors', doctorController.getAllDoctors);
+    router.post('/api/save-info-doctor', doctorController.saveInfoDoctor);
+
 
     return app.use("/", router);
 }
