06b8e4b3c942e831bb1df3d67c9fa05be9d8da5e
ahihi <bdsthocu20@gmail.com>
2023-07-11 11:57:44 +0700

    #84

--- src/route/web.js
+++ src/route/web.js
@@ -1,7 +1,8 @@
 import express from "express";
 import homeController from "../controllers/homeController";
-import userController from "../controllers/userController"
-import doctorController from "../controllers/doctorController"
+import userController from "../controllers/userController";
+import doctorController from "../controllers/doctorController";
+import patientControlller from "../controllers/patientController"
 
 let router = express.Router();
 
@@ -30,6 +31,8 @@ let initWebRoutes = (app) => {
     router.get('/api/get-schedule-doctor-by-date', doctorController.getScheduleByDate);
     router.get('/api/get-extra-info-doctor-by-id', doctorController.getExtraInfoDoctorById);
     router.get('/api/get-profile-doctor-by-id', doctorController.getProfileDoctorById);
+
+    router.post('/api/patient-book-appointment', patientControlller.postBookAppointment);
 
     return app.use("/", router);
 }
